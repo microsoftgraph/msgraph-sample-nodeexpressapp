9591331afd3252696229f245ae4c75c76d343fd4
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2020-06-19 17:17:47 +0900

    Add bot to show the sign-in link

--- demo/graph-tutorial/app.js
+++ demo/graph-tutorial/app.js
@@ -8,6 +8,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var flash = require('connect-flash');
+
+const { BotFrameworkAdapter } = require('botbuilder');
+const { EchoBot } = require('./bot');
+
 require('dotenv').config();
 
 var passport = require('passport');
@@ -169,6 +173,46 @@ app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/calendar', calendarRouter);
 app.use('/users', usersRouter);
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about how bots work.
+const adapter = new BotFrameworkAdapter({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword,
+    // channelService: process.env.ChannelService,
+    // openIdMetadata: process.env.BotOpenIdMetadata
+});
+
+// Catch-all for errors.
+adapter.onTurnError = async (context, error) => {
+    // This check writes out errors to console log .vs. app insights.
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
+    console.error(`\n [onTurnError] unhandled error: ${ error }`);
+
+    // Send a trace activity, which will be displayed in Bot Framework Emulator
+    await context.sendTraceActivity(
+        'OnTurnError Trace',
+        `${ error }`,
+        'https://www.botframework.com/schemas/error',
+        'TurnError'
+    );
+
+    // Send a message to the user
+    await context.sendActivity('The bot encounted an error or bug.');
+    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+};
+
+// Create the main dialog.
+const myBot = new EchoBot();
+
+// Listen for incoming requests.
+app.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async (context) => {
+        // Route to main dialog.
+        await myBot.run(context);
+    });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ demo/graph-tutorial/bot.js
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+require('dotenv').config();
+
+const { ActivityHandler } = require('botbuilder');
+
+class EchoBot extends ActivityHandler {
+    constructor() {
+        super();
+        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+        this.onMessage(async (context, next) => {
+            await context.sendActivity(`You said **${ context.activity.text.toUpperCase() }** at ${new Date()}`);
+
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+
+        this.onMembersAdded(async (context, next) => {
+            const membersAdded = context.activity.membersAdded;
+            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+                if (membersAdded[cnt].id !== context.activity.recipient.id) {
+                    await context.sendActivity(`Hello and welcome! <${process.env.ORIGIN_URL}/auth/signin>`);
+                }
+            }
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+    }
+}
+
+module.exports.EchoBot = EchoBot;
