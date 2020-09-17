e69b53853b6e13403ec3940a903182eb0c48022f
Jason Johnston <jasonjoh@users.noreply.github.com>
2020-09-17 12:42:18 -0400

    Updated create app step

--- demo/graph-tutorial/app.js
+++ demo/graph-tutorial/app.js
@@ -3,11 +3,44 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// <SessionSnippet>
+// Session middleware
+// NOTE: Uses default in-memory session store, which is not
+// suitable for production
+app.use(session({
+  secret: 'your_secret_value_here',
+  resave: false,
+  saveUninitialized: false,
+  unset: 'destroy'
+}));
+
+// Flash middleware
+app.use(flash());
+
+// Set up local vars for template layout
+app.use(function(req, res, next) {
+  // Read any flashed errors and save
+  // in the response locals
+  res.locals.error = req.flash('error_msg');
+
+  // Check for simple error string and
+  // convert to layout's expected format
+  var errs = req.flash('error');
+  for (var i in errs){
+    res.locals.error.push({message: 'An error occurred', debug: errs[i]});
+  }
+
+  next();
+});
+// </SessionSnippet>
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- demo/graph-tutorial/routes/index.js
+++ demo/graph-tutorial/routes/index.js
@@ -1,9 +1,18 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// <IndexRouterSnippet>
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let params = {
+    active: { home: true }
+  };
+
+  res.render('index', params);
 });
 
 module.exports = router;
+// </IndexRouterSnippet>
